8fd5373ca3050061693dbe342abeff95356552ae
Sakshi Mulekar <sakshimulekar2017@gmail.com>
2023-06-24 18:11:25 +0530

    authentication done

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const cors=require("cors")
+const { connection } = require("./db")
+const { userRoute } = require("./routes/userRoute.route")
+require("dotenv").config()
+const app=express()
+app.use(express.json())
+app.use(cors())
+app.use("/users",userRoute)
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log(`${process.env.port}`)
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
+
+
